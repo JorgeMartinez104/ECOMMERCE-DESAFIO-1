af6531c22d135bbb85eb1777092f84a78e0663ac
JorgeMartinez104 <martinezj1975@gmail.com>
2023-09-16 22:20:11 -0300

    falta la parte de back

---
+++ js/products.js
@@ -0,0 +1,23 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Bugatti Chiron Super Sport",
+        price: 490,
+        quanty: 1,
+        img: "./media/bugatti.png",
+    },
+    {
+        id: 2,
+        productName: "SSC Tuatara",
+        price: 460,
+        quanty: 1,
+        img: "./media/tuatara.png",
+    },
+    {
+        id: 3,
+        productName: "Koenigsegg Agera",
+        price: 447,
+        quanty: 1,
+        img: "./media/agera.png",
+    },
+];
